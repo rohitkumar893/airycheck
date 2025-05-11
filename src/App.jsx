715cd62596b1575127f37885a9b641a1a2ff4ebc
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-05-12 00:03:33 +0530

    modified search bar

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ const App = () => {
     <div className='flex justify-center bg-orange-200 items-center h-full w-full'>
       
       <div className='container bg-orange-300 w-[90%] h-[90%] flex flex-col rounded-2xl justify-center'>
-        <div className='flex justify-center h-[130px] w-full border-2'>
-          <label htmlFor='city' className='text-[30px] font-medium'>ENTER CITY:</label>
-          <input type='text' placeholder='enter city name' name='city'></input>
+        <div className='flex flex-col items-center h-[130px] w-full gap-[12px]'>
+          <label htmlFor='city' className='text-[30px] font-medium'>SEARCH:</label>
+          <input type='text' placeholder='Enter city name' name='city' className='border-2 rounded-md p-[10px]'></input>
         </div>
         <div className='flex flex-col md:flex-row justify-around'>
           <div className='first p-[100px]'>
