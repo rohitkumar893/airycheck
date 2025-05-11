50df8c900b99197b82ca75468eba03f56068254c
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-05-12 00:08:57 +0530

    Added button

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import './App.css'
 
 const App = () => {
+
+  const[state, setState] = useState("")
+
   return (
-    <div className='flex justify-center bg-orange-200 items-center h-full w-full'>
+    <div className='flex justify-center bg-orange-300 items-center h-full w-full'>
       
-      <div className='container bg-orange-300 w-[90%] h-[90%] flex flex-col rounded-2xl justify-center'>
+      <div className='container bg-orange-200 w-[90%] h-[90%] flex flex-col rounded-2xl '>
         <div className='flex flex-col items-center h-[130px] w-full gap-[12px]'>
-          <label htmlFor='city' className='text-[30px] font-medium'>SEARCH:</label>
+          <label htmlFor='city' className='text-[30px] font-medium mt-[50px]'>SEARCH:</label>
           <input type='text' placeholder='Enter city name' name='city' className='border-2 rounded-md p-[10px]'></input>
+          <button type='submit'>GO</button>
         </div>
         <div className='flex flex-col md:flex-row justify-around'>
           <div className='first p-[100px]'>
